974d2079abc827764eb33ef2cf2e8ee17bc93ce1
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2025-12-13 16:30:15 +0100

    Level generator supporting Teleport

--- src/main/scala/de/kappmeier/asnarc/LevelGenerator.scala
+++ src/main/scala/de/kappmeier/asnarc/LevelGenerator.scala
@@ -1,32 +1,31 @@
 package de.kappmeier.asnarc
 
 import java.util.Base64
-
 import de.kappmeier.asnarc.board.{Direction, Point}
 import de.kappmeier.asnarc.board.Direction._
-import de.kappmeier.asnarc.elements.{Element, Wall}
+import de.kappmeier.asnarc.elements.{Element, Teleport, Wall}
 
 import scala.collection.immutable.{HashMap, Set}
 import scala.collection.mutable
 
 /**
-  * A factory that can create [[SnakeGame]] boards to be used to play. The levels are defined in a text definition, either
-  * in clear text or Base 64 encoded. Clear text encoding can introduce encoding problems, such that in cases where UTF-8
-  * is not available Base 64 should be preferred.
-  *
-  * Each element is represented as a comma separated 3 tuple `x,y,c` where `x` and `y` represent the coordinate of a field and
-  * `c` denotes the type of element stored at the location. An example is `0,0,┌`, which places an element into the upper left
-  * corner that is connected to the right and bottom elements, i.e. it is actually a corner.
-  *
-  * The whole description of the board consists of a list of `;` separated list of the aforementioned 3 tuples, i.e.
-  * `0,0,┌;59,39,┘;0,1,│`
-  *
-  * The following types are available: '─', '│', '┌', '┐', '┘', '└', '├', '┬', '┤', '┴', '┼', ' '
-  *
-  * All elements but the last one represent a filled block which is supposed to be connected with 1 or more neighbour blocks.
-  * The last block represents an empty field. It is not required to submit empty fields, every field not specified is assumed
-  * to be empty.
-  */
+ * A factory that can create [[de.kappmeier.asnarc.game.AsnarcGame]] boards to be used to play. The levels are defined
+ * in a text definition, either in clear text or Base 64 encoded. Clear text encoding can introduce encoding problems,
+ * such that in cases where UTF-8 is not available Base 64 should be preferred.
+ *
+ * Each element is represented as a comma separated 3 tuple `x,y,c` where `x` and `y` represent the coordinate of a
+ * field and `c` denotes the type of element stored at the location. An example is `0,0,┌`, which places an element into
+ * the upper left corner that is connected to the right and bottom elements, i.e. it is actually a corner.
+ *
+ * The whole description of the board consists of a list of `;` separated list of the aforementioned 3 tuples, i.e.
+ * `0,0,┌;59,39,┘;0,1,│`
+ *
+ * The following types are available: '─', '│', '┌', '┐', '┘', '└', '├', '┬', '┤', '┴', '┼', ' '
+ *
+ * All elements but the last one represent a filled block which is supposed to be connected with 1 or more neighbour
+ * blocks. The last block represents an empty field. It is not required to submit empty fields, every field not
+ * specified is assumed to be empty.
+ */
 case class LevelGenerator() {
   /** The decoder. */
   val decoder = Base64.getDecoder
@@ -39,28 +38,31 @@ case class LevelGenerator() {
   }
 
   /**
-    * Generates the [[Wall]] elements belonging to a level given as `String`.
-    *
-    * @param levelInput the input as string
-    * @return the static initial definition of a level
-    */
+   * Generates the [[Wall]] elements belonging to a level given as `String`.
+   *
+   * @param levelInput the input as string
+   * @return the static initial definition of a level
+   */
   def generateBoard(levelInput: String): mutable.Queue[Element] = {
     val input: Seq[(Point, Char)] = breakBlocks(levelInput).map(x => breakBlock(x))
     val builder: mutable.Builder[Element, mutable.Queue[Element]] = mutable.Queue.newBuilder
-    builder ++= input.map { case (location, element) => Wall(location, LevelGenerator.BoundMap(element)) }
+    builder ++= input.map {
+      case (location, element) if element == 'T' => Teleport(location, Set())
+      case (location, element) => Wall(location, LevelGenerator.BoundMap(element))
+    }
     builder.result()
   }
 
   /**
-    * Breaks the level into parts for each row. The level description contains the information for each row separated
-    * by `;`.
-    *
-    * @param input level description for multiple lines
-    * @return
-    */
-  def breakBlocks(input: String): Seq[String] = input.split(";")
+   * Breaks the level into parts for each row. The level description contains the information for each row separated
+   * by `;`.
+   *
+   * @param input level description for multiple lines
+   * @return
+   */
+  private def breakBlocks(input: String): Seq[String] = input.split(";")
 
-  def breakBlock(input: String): (Point, Char) = {
+  private def breakBlock(input: String): (Point, Char) = {
     val parts = input.split(",")
     (Point(parts(0).toInt, parts(1).toInt), parts(2).toCharArray.apply(0))
   }
@@ -68,15 +70,15 @@ case class LevelGenerator() {
 
 object LevelGenerator {
   /**
-    * Base64 encoding of an empty level.
-    */
+   * Base64 encoding of an empty level.
+   */
   val EmptyLevel = "MCwwLCA="
 
   /**
-    * Maps the level description `Char`s to the direction with which the corresponding [[Wall]] will be connected
-    * to.
-    */
-  val BoundMap: HashMap[Char, Set[Direction]] = collection.immutable.HashMap[Char, Set[Direction]](
+   * Maps the level description `Char`s to the direction with which the corresponding [[Wall]] will be connected
+   * to.
+   */
+  private val BoundMap: HashMap[Char, Set[Direction]] = collection.immutable.HashMap[Char, Set[Direction]](
     '─' -> Set(Direction.Left, Direction.Right),
     '│' -> Set(Direction.Down, Direction.Up),
     '┌' -> Set(Direction.Down, Direction.Right),
@@ -89,5 +91,5 @@ object LevelGenerator {
     '┴' -> Set(Direction.Left, Direction.Right, Up),
     '┼' -> Set(Direction.Down, Direction.Left, Direction.Right, Direction.Up),
     ' ' -> Set()
-  )
+    )
 }
