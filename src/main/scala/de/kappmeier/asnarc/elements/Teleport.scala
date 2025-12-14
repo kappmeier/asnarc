cde2b15b2f11ebb2d7770141cd6758d05d3c5ee3
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2025-12-14 23:41:41 +0100

    Updated the teleport element

--- src/main/scala/de/kappmeier/asnarc/elements/Teleport.scala
+++ src/main/scala/de/kappmeier/asnarc/elements/Teleport.scala
@@ -3,4 +3,8 @@ package de.kappmeier.asnarc.elements
 import de.kappmeier.asnarc.board.Direction.Direction
 import de.kappmeier.asnarc.board.Point
 
-case class Teleport(p: Point, connects: Set[Direction]) extends Element
+case class Teleport(
+                     p: Point,
+                     connects: Set[Direction],
+                     target: Option[Point]
+                   ) extends Element
