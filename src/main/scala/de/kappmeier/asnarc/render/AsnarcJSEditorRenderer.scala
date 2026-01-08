a0f920e6957d1918e51481f0cb5db9ff01e17187
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2026-01-08 22:50:21 +0100

    Use canvas for rendering editor with same engine as game

--- src/main/scala/de/kappmeier/asnarc/render/AsnarcJSEditorRenderer.scala
+++ src/main/scala/de/kappmeier/asnarc/render/AsnarcJSEditorRenderer.scala
@@ -1,27 +1,20 @@
 package de.kappmeier.asnarc.render
 
-import de.kappmeier.asnarc.board.AsnarcBoard
-import de.kappmeier.asnarc.elements.Element
-import de.kappmeier.asnarc.render.AsnarcJSRenderer.stripSimpleName
-import de.kappmeier.asnarc.render.localization.AsnarcLocalization
-import org.scalajs.dom
 import org.scalajs.dom.html
 
+import de.kappmeier.asnarc.board.AsnarcBoard
+import de.kappmeier.asnarc.render.localization.AsnarcLocalization
+
 /**
+
   * Draws the Asnarc game into a canvas.
   *
   * @param boardCanvas the HTML canvas element for the game board
-  * @param detailsCanvas the HTML canvas element for showing element details
   * @param loc localization for text rendering
   * @param config renderer configuration with block size and derived values
   */
-class AsnarcJSEditorRenderer(boardCanvas: html.Canvas, detailsCanvas: html.Canvas,
-                             loc: AsnarcLocalization, config: AsnarcJSRenderer)
+class AsnarcJSEditorRenderer(boardCanvas: html.Canvas, loc: AsnarcLocalization, config: AsnarcJSRenderer)
     extends AbstractAsnarcJSRenderer(boardCanvas, loc, config) {
-
-  val rendererDetails: dom.CanvasRenderingContext2D = detailsCanvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
-
-  val scale: Double = detailsCanvas.width.toDouble / config.Size
 
   def renderBoard(board: AsnarcBoard, info: String): Unit = {
     clear()
@@ -29,21 +22,11 @@ class AsnarcJSEditorRenderer(boardCanvas: html.Canvas, detailsCanvas: html.Canva
     renderInfo(board, info, "")
   }
 
-  def highlight(x: Int, y: Int) = {
+  def highlight(x: Int, y: Int): Unit = {
     highlightElement(x, y)
   }
 
-  def highlightElement(element: Element): Unit = {
-    rendererDetails.clearRect(0, 0, detailsCanvas.width, detailsCanvas.height)
-    if (element.p.x >= 0 && element.p.y >= 0) {
-      val color = AsnarcJSRenderer.DrawColors.getOrElse(stripSimpleName(element.getClass.getSimpleName), "black")
-      rendererDetails.fillStyle = color
-      AbstractAsnarcJSRenderer.fillElementAt(rendererDetails, 0, 0, element, config, scale)
-      highlightElement(element.p.x, element.p.y)
-    }
-  }
-
-  def highlightElement(xPosition: Int, yPosition: Int): Unit = {
+  private def highlightElement(xPosition: Int, yPosition: Int): Unit = {
     renderer.strokeStyle = "red"
     renderer.lineWidth = 2
     val x: Int = xPosition * config.Size
